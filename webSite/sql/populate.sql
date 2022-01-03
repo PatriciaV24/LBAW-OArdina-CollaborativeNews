711d83ba27f180fad03a8957cb7a31145f15afb2
up201805238 <up201805238@fc.up.pt>
2022-01-03 14:35:20 +0000

    fazer inserts

--- webSite/sql/populate.sql
+++ webSite/sql/populate.sql
@@ -6,16 +6,17 @@ VALUES
   ('fringilla','aptent.taciti@outlook.net','senectus','/src/photos/test.jpg','817282354'),
   ('mauris','ligula@protonmail.org','nunc','/src/photos/test.jpg','667894517');
 
+/*Criar um administrador*/
+UPDATE utilizador 
+SET permissao = 'a' 
+WHERE id = 2;
 
-UPDATE utilizador /*Criar um administrador*/
-SET usertype = 'a'
-WHERE users_id = 2;
+/*Criar um user banido*/
+UPDATE utilizador 
+SET permissao = 'b'; 
+WHERE id = 5;
 
-UPDATE utilizador /*Criar um user banido*/
-SET usertype = 'b';
-WHERE users_id = 5;
-
-INSERT INTO noticias(author_id,titulo,descricao)
+INSERT INTO noticia(autor_id,titulo,descricao)
 VALUES
   (3,'a felis','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at lacus. Quisque purus sapien, gravida non, sollicitudin a, malesuada id, erat. Etiam vestibulum massa rutrum magna. Cras convallis convallis dolor. Quisque tincidunt pede ac urna. Ut tincidunt vehicula risus. Nulla eget metus eu erat semper rutrum. Fusce dolor quam, elementum at, egestas a, scelerisque sed, sapien. Nunc pulvinar arcu et pede. Nunc sed orci lobortis augue scelerisque mollis. Phasellus libero mauris, aliquam eu, accumsan sed, facilisis'),
   (3,'pretium neque. Morbi','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at lacus. Quisque purus sapien, gravida non, sollicitudin a, malesuada id, erat. Etiam vestibulum massa rutrum magna. Cras convallis convallis dolor. Quisque tincidunt pede ac urna. Ut tincidunt vehicula risus. Nulla eget metus eu erat semper rutrum. Fusce dolor quam, elementum at, egestas a, scelerisque sed, sapien. Nunc pulvinar'),
@@ -23,21 +24,25 @@ VALUES
   (4,'Class aptent taciti sociosqu','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at lacus. Quisque purus sapien, gravida non, sollicitudin a, malesuada id, erat. Etiam vestibulum massa rutrum magna. Cras convallis convallis dolor. Quisque tincidunt pede ac urna. Ut tincidunt vehicula risus. Nulla eget metus eu erat semper rutrum. Fusce dolor quam, elementum at, egestas a, scelerisque'),
   (2,'massa. Mauris','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at lacus. Quisque purus sapien, gravida non, sollicitudin a, malesuada id, erat. Etiam vestibulum massa rutrum magna. Cras convallis convallis dolor. Quisque tincidunt pede ac urna. Ut tincidunt vehicula risus. Nulla eget metus eu erat semper rutrum. Fusce dolor quam, elementum at, egestas a, scelerisque sed, sapien. Nunc pulvinar arcu et pede. Nunc sed');
 
-UPDATE noticias
-SET titulo = 'Ola Pessoa'
+UPDATE noticia 
+SET titulo = 'Porto é campeão nacional!'
 WHERE noticia_id = 3;
 
-INSERT INTO tags(nome)
+INSERT INTO tag(nome)
 VALUES
     ('Desporto'),
     ('Nacional'),
     ('Local'),
     ('Mundo');
 
+UPDATE tag 
+SET prioridade=1;
+WHERE nome = 'Nacional'; 
+
 DELETE FROM tags
 WHERE tag_id = 2;
 
-INSERT INTO comentarios (author_id,noticias_id,comentario)
+INSERT INTO comentario(autor_id,not_id,texto)
 VALUES
   (3,3,'mi felis, adipiscing fringilla, porttitor'),
   (3,4,'Nulla tempor augue ac ipsum. Phasellus vitae mauris'),
@@ -45,7 +50,13 @@ VALUES
   (1,1,'Morbi non sapien molestie orci'),
   (1,3,'turpis vitae');
 
-INSERT INTO noti_tags(noticias_id, tag_id)
+/*VALUES TEM DE SER IGUAIS AOS VALUES DA TABELA comentario*/
+/*autor_id -> é o autor da noticia da not_id (QUE RECEBE A NOTIFICAÇAO)*/
+INSERT INTO n_comentario(autor_id,com_id)
+VALUES
+
+
+INSERT INTO categoria(not_id, tag_id)
 VALUES
     (1,2),
     (2,1),
@@ -53,23 +64,76 @@ VALUES
     (4,3),
     (5,4);
 
-INSERT INTO seguir(users_id, follow_id)
+INSERT INTO faq(autor_id, questao, resposta)
+VALUES 
+    (2, 'Como inserir uma notícia ?', 'Nulla tempor augue ac ipsum. Phasellus vitae mauris');
+
+
+INSERT INTO imagem(legenda,imag_path,not_id)
+VALUES
+
+INSERT INTO vot_com(com_id,autor_id,tipo)
+VALUES
+
+/*VALUES TEM DE SER IGUAIS AOS VALUES DA TABELA vot_com*/
+/*autor_id -> é o autor do comentário da com_id (QUE RECEBE A NOTIFICAÇAO)*/
+INSERT INTO n_vot_com(autor_id,voto_id)
+VALUES
+
+INSERT INTO vot_not(not_id,autor_id,tipo)
+VALUES
+
+
+/*VALUES TEM DE SER IGUAIS AOS VALUES DA TABELA vot_not*/
+/*autor_id -> é o autor da noticia da not_id (QUE RECEBE A NOTIFICAÇAO)*/
+INSERT INTO n_vot_not(autor_id,voto_id)
+VALUES
+
+INSERT INTO fav_not(autor_id,not_id)
+VALUES
+
+INSERT INTO fav_com(autor_id,comentario_id)
+VALUES
+
+INSERT INTO fav_tag(autor_id,tag_id)
+VALUES
+
+INSERT INTO info_seguidor(followed_id, infos_id)
 VALUES
     (1,3),
     (3,2),
     (4,1),
     (3,1);
 
+/*VALUES TEM DE SER IGUAIS AOS VALUES DA TABELA info_seguidor*/
+INSERT INTO n_seguidor(followed_id,infos_id)
+VALUES 
+
 INSERT INTO texto_report(report)
 VALUES
     ('O utilizador foi agressivo com outro e além de ser agressivo tentou ameaçar diversos utilizadores');
 
-INSERT INTO report_u(author_id, users_id, tipo_rep)
+INSERT INTO report_u(autor_id, uti_id, tiporesp_id)
 VALUES
     (3,2,1);
 
+INSERT INTO report_u(autor_id, uti_id, tiporesp_id, resolvido)
+VALUES
+    (,,,TRUE);
 
-INSERT INTO faq(author_id, question, answer)
+/*VALUES TEM DE SER IGUAIS AOS VALUES DA TABELA report_u MAS com resolvido a TRUE*/
+INSERT INTO n_uti_bloq (bloq_id, motivo)
 VALUES 
-    (2, 'Como inserir uma notícia ?', 'Nulla tempor augue ac ipsum. Phasellus vitae mauris');
-s
+
+INSERT INTO report_n(autor_id, not_id, tiporesp_id)
+VALUES
+    (3,2,1);
+
+INSERT INTO report_c(autor_id, comentario_id, tiporesp_id)
+VALUES
+    (3,2,1);
+
+INSERT INTO publicidade(imagem)
+VALUES 
+
+
--- webSite/sql/queries.sql
+++ webSite/sql/queries.sql
@@ -5,8 +5,8 @@
 BEGIN TRANSACTION;
 SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;
  
-INSERT INTO noticias (author_id, titulo, descricao, date)
-VALUES ($author_id, $titulo, $descricao, $date);
+INSERT INTO noticia (autor_id, titulo, descricao, date)
+VALUES ($autor_id, $titulo, $descricao, $date);
  
 COMMIT;
 -------------------------------
@@ -17,11 +17,11 @@ BEGIN TRANSACTION;
 SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;
 
 
-INSERT INTO noticias (author_id, titulo, descricao, date)
-VALUES ($author_id, $titulo, $descricao, $date);
+INSERT INTO noticias (autor_id, titulo, descricao, date)
+VALUES ($autor_id, $titulo, $descricao, $date);
 
-INSERT INTO comentario (author_id, noticia_id, comentario)
-VALUES ($author_id, currval('noticia_id_seq'), $comentario);
+INSERT INTO comentario (autor_id, noticia_id, comentario)
+VALUES ($autor_id, currval('noticia_id_seq'), $comentario);
 
 COMMIT;
 
@@ -32,8 +32,8 @@ COMMIT;
 BEGIN TRANSACTION
 SET TRANSACTION ISOLATION LEVEL REPEATABLE READ:
 
-INSERT INTO report_u (author_id, users_id, tipo_rep, resolucao)
-VALUES($author_id, $titulo, $descricao, $date);
+INSERT INTO report_u (autor_id, users_id, tipo_rep, resolucao)
+VALUES($autor_id, $titulo, $descricao, $date);
 
 COMMIT;
 -------------------------------------------
@@ -42,8 +42,8 @@ COMMIT;
 BEGIN TRANSACTION
 SET TRANSACTION ISOLATION LEVEL REPEATABLE READ:
 
-INSERT INTO report_c (author_id, comentario_id, tipo_rep, resolucao)
-VALUES($author_id, $comentario_id, $descricao, $date);
+INSERT INTO report_c (autor_id, comentario_id, tipo_rep, resolucao)
+VALUES($autor_id, $comentario_id, $descricao, $date);
 
 COMMIT;
 -------------------------------------------
@@ -53,8 +53,8 @@ COMMIT;
 BEGIN TRANSACTION
 SET TRANSACTION ISOLATION LEVEL REPEATABLE READ:
 
-INSERT INTO report_n (author_id, noticia_id, tipo_rep, resolucao)
-VALUES($author_id, $noticia_id, $descricao, $date);
+INSERT INTO report_n (autor_id, noticia_id, tipo_rep, resolucao)
+VALUES($autor_id, $noticia_id, $descricao, $date);
 
 COMMIT;
 -------------------------------------------
