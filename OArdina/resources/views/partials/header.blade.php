849b936cab74960aee684a1af1168f1af4d8bcf4
shibuizzz <antoniocampello98@hotmail.com>
2022-01-13 20:32:55 +0000

    header icons + fixes

--- OArdina/resources/views/partials/header.blade.php
+++ OArdina/resources/views/partials/header.blade.php
@@ -8,10 +8,13 @@
 		</div>
 		<div id="user_container">
 			<div id="search">
-				<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M15.853 16.56c-1.683 1.517-3.911 2.44-6.353 2.44-5.243 0-9.5-4.257-9.5-9.5s4.257-9.5 9.5-9.5 9.5 4.257 9.5 9.5c0 2.442-.923 4.67-2.44 6.353l7.44 7.44-.707.707-7.44-7.44zm-6.353-15.56c4.691 0 8.5 3.809 8.5 8.5s-3.809 8.5-8.5 8.5-8.5-3.809-8.5-8.5 3.809-8.5 8.5-8.5z"/></svg>
+				@include('partials.svg.icons.search')
 			</div>
-			<div id="user_box"><a href="{{ url('/login') }}">Iniciar Sessão</a></div>
-			<div id="user_box"><a href="{{ url('/signup') }}">Criar Conta</a></div>
+			<div id="user_box">
+				@include('partials.svg.icons.user')
+				{{-- <a href="{{ url('/login') }}">Iniciar Sessão</a>
+				<a href="{{ url('/signup') }}">Criar Conta</a> --}}
+			</div>
 		</div>
 	</div>
 	<div id="navbar">
