f75610949257040266373ba8ff173299bdd14e52
shibuizzz <antoniocampello98@hotmail.com>
2022-01-21 16:46:35 +0000

    contacts page

--- OArdina/resources/views/pages/contacts.blade.php
+++ OArdina/resources/views/pages/contacts.blade.php
@@ -4,12 +4,36 @@
 
 @section('content')
 
-<main class="container-xl text-dark ">  
+<main>  
 
-    <div class="container-xl pt-3">   
-        <h1 class="text-dark  text-center py-4">Contactos</h1>
-        <p class="text-dark  text-center py-4"> 
-            COLOCAR UM TEXTOZINHO A FALAR DO ARDINA
+    <div class="pt-3">   
+        <h1 class="text-dark text-center py-4">Contactos</h1>
+        <p class="py-3">
+
+            <div class="row text-dark text-center">
+                <span style="font-size: 22px;">Alguma dúvida ou sugestão? Entre em contacto connosco!</span>
+            </div>
+            <div class="row text-center py-4" style="color: #730909; font-size: 20px; font-weight: bold;">
+                <span>oardina@ardina.com</span>
+            </div>
+
+
+            <div class="row text-light py-5">
+                <div class="row bg-dark" style="padding: 0;">
+                    <div class="col-lg-6 py-4" style="font-size: 20px; padding-left: 50px; position: relative;top: 8%;">
+                        <p class="text-light text-decoration-none fs-7">Alguns links úteis</p>
+                    </div>
+                    <div class="col-lg-6 py-4" style="background-color: #5c1c1c; padding-left: 40px;">
+                        <div class="row"> 
+                            <a href="{{ route('faq') }}" class="text-light text-decoration-none fs-6 clickable">FAQ</a>
+                        </div>
+                        <div class="row">
+                            <a href="{{ route('about') }}" class="text-light text-decoration-none fs-6 clickable">Sobre Nós</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </p>
         <div class="row text-dark  pt-4">
             @include('partials.about.creator_card', ['number' => 'up201704987@fc.up.pt', 'name' => 'António Campelo'])
@@ -18,6 +42,7 @@
             @include('partials.about.creator_card', ['number' => 'up201805238@fc.up.pt', 'name' => 'Patrícia Vieira'])
         </div>
     </div>
+
 </main>
 
 @endsection
